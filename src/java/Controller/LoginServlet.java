7b6e04003bdc0b025c6e417a547b95d4f36e20c2
MT <vominhthang061003@gmail.com>
2023-07-06 17:59:37 +0700

    update header staff

--- src/java/Controller/LoginServlet.java
+++ src/java/Controller/LoginServlet.java
@@ -81,8 +81,9 @@ public class LoginServlet extends HttpServlet {
                     TrainerDAO trainerdao = new TrainerDAO();
                     TrainerSP trainer = trainerdao.getTrainerInfo(user.getUser_id());
                     session.setAttribute("LOGIN_USER", trainer);
-                    url = "Trainer_PrivateConsultation_List.jsp";
+                    url = "Trainer_Dashboard.jsp";
                     // url = "Trainer_Tracking";
+
                 } else if (user.getRole().equals("staff")) {
                     UserDAO staffdao = new UserDAO();
                     StaffDTO staff = staffdao.getStaffInfo(user.getUser_id());
@@ -91,6 +92,7 @@ public class LoginServlet extends HttpServlet {
                     url = "Staff_Dashboard.jsp";
                     //   url="trainer_demo.jsp";
                     //  url = "staff_birdCourseForm.jsp";
+
                 } else if (user.getRole().equals("admin")) {
                     url = "";
                 }
