8e5fe59de692057f06a93030de151fb77d21137a
MT <vominhthang061003@gmail.com>
2023-07-10 20:41:34 +0700

    Revert "first commit"
    This reverts commit 92b460205893e81402a6096671bd3de654081a78.

--- build/web/js/private_consultation.js
+++
@@ -1,8 +0,0 @@
-const toggleSubnav = document.querySelector(".subnav-toggle")
-const subnav = document.querySelector(".subnav")
-const triangle = document.querySelector(".triangle")
-
-toggleSubnav.addEventListener('click', function(){
-    subnav.style.display = subnav.style.display === 'none' ? 'block' : 'none'
-    triangle.style.display = triangle.style.display === 'none' ? 'block' : 'none'
-})
